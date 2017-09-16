526d13e4d49c44accb1b2fd142a666a0be87e093
Isaac Snow <isaacjsnow@gmail.com>
2017-09-16 13:07:20 -0700

    edit scripts

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,49 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
-docker build -t ijsnow/me .
+NAME="ijsnow/me"
+
+echo '>>> Get old container id'
+CID=$(docker ps | grep "$NAME" | awk '{print $1}')
+echo $CID
+
+echo '>>> Building new image'
+# Due to a bug in Docker we need to analyse the log to find out if build passed (see https://github.com/dotcloud/docker/issues/1875)
+docker build ./deploy | tee /tmp/docker_build_result.log
+RESULT=$(cat /tmp/docker_build_result.log | tail -n 1)
+if [[ "$RESULT" != *Successfully* ]];
+then
+  exit -1
+fi
+
+echo '>>> Tagging new image'
+NEW=$(sudo docker ps -a -q | head -n 1)
+docker commit $NEW $NAME
+
+
+echo '>>> Stopping old container'
+if [ "$CID" != "" ];
+then
+  docker stop $CID
+fi
+
+
+echo '>>> Restarting docker'
+sudo service docker restart
+sleep 5
+
+
+echo '>>> Starting new container'
+docker run -p 3000:3000 -d $NAME
+
+
+echo '>>> Cleaning up containers'
+docker ps -a | grep "Exit" | awk '{print $1}' | while read -r id ; do
+  docker rm $id
+done
+
+
+echo '>>> Cleaning up images'
+docker images | grep "^<none>" | head -n 1 | awk 'BEGIN { FS = "[ \t]+" } { print $3 }'  | while read -r id ; do
+  docker rmi $id
+done
